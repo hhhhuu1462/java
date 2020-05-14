24a09b1240d86e3b826a103c11c3fc43870b307d
hhhhu <hhhhu@LAPTOP-J6L8V7RE>
2020-05-15 01:06:54 +0900

    자바의 정석 Chapter5 확인문제

---
+++ 자바의정석/src/Chapter5/Ex04.java
@@ -0,0 +1,28 @@
+package Chapter5;
+
+public class Ex04 {
+	public static void main(String[] args) {
+		// 2차원 배열 arr에 담긴 모든 값의 총합과 평균
+		int[][] arr = {
+				{ 5, 5, 5, 5, 5},
+				{10,10,10,10,10},
+				{20,20,20,20,20},
+				{30,30,30,30,30}
+		};
+
+		int total = 0;
+		float average = 0;
+
+		int count = 0;
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				total += arr[i][j];
+				count++;
+			}
+		}
+		average = (float)total / count;
+
+		System.out.println("total="+total);
+		System.out.println("average="+average);
+	}
+}
