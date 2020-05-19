48b8dbe722c6b5c165bb4448b8ccd94a64ab0a78
heesungLee <hhhhuu1462@gmail.com>
2020-05-19 13:54:24 +0900

    네트워크기초.연결요청_수락

---
+++ IO 기반 입출력 및 네트워킹/src/네트워크기초/연결요청_수락/ServerEx.java
@@ -0,0 +1,28 @@
+package 네트워크기초.연결요청_수락;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerEx {
+	public static void main(String[] args) {
+		ServerSocket serverSocket = null;
+		try {
+			serverSocket = new ServerSocket();		
+			serverSocket.bind(new InetSocketAddress("localhost", 5001));
+			while(true) {
+				System.out.println( "[연결 기다림]");
+				Socket socket = serverSocket.accept();
+				InetSocketAddress isa = (InetSocketAddress) socket.getRemoteSocketAddress();
+				System.out.println("[연결 수락함] " + isa.getHostName());
+			}
+		} catch(Exception e) {}
+		
+		if(!serverSocket.isClosed()) {
+			try {
+				serverSocket.close();
+			} catch (IOException e1) {}
+		}
+	}
+}
