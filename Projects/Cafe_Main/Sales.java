8724aa0308969132c1bc93127a3feabcae519475
heesungLee <hhhhuu1462@gmail.com>
2020-05-21 16:13:58 +0900

    a

--- Projects/Cafe_Main/Sales.java
+++ Projects/Cafe_Main/Sales.java
@@ -2,24 +2,25 @@ package Cafe_Main;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class Sales extends JFrame {
-	
+
 	public Sales() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 600, 340);
 		getContentPane().setLayout(null);
-		
+
 		List list = new List();
 		list.setBounds(0, 0, 584, 302);
 		getContentPane().add(list);
-		
+
+		setResizable(false);
 		setVisible(true);
 	}
-	
+
 	public static void main(String[] args) {
-		Sales frame = new Sales();
-		frame.setResizable(false);
-		frame.setVisible(true);
+		Sales frame = new Sales();		
 	}
 }
