da6d6b5bcc99493f2dd8826af5a52f6497778789
hhhhuu1462 <42793644+hhhhuu1462@users.noreply.github.com>
2020-05-29 18:08:39 +0900

    Update Sales.java

--- Projects/Cafe_Main/Sales.java
+++ Projects/Cafe_Main/Sales.java
@@ -1,34 +1,36 @@
 package Cafe_Main;
 
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+
+import java.awt.Panel;
+import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
 public class Sales extends JFrame {
 	
-	String[] title = { "No", "ªÛ«∞∏Ì", "ºˆ∑Æ", "¥‹∞°", "±›æ◊" };
-	String [] ColName = {"No", "∏ﬁ¥∫","ºˆ∑Æ","∞°∞›"};
+	String [] ColName = {"MenuCode", "Î©îÎâ¥", "Í∞ÄÍ≤©", "ÎÇ†Ïßú"};
 	String [][] Data ;	
-	int count =1;
-	
+	int count =1;	
 	DefaultTableModel model = new DefaultTableModel(Data,ColName);
 	JTable menuTable = new JTable(model);
 	JScrollPane menuScroll = new JScrollPane();
-	
+
 	public Sales() {
-		super("∏≈√‚∏Ò∑œ");
+		super("Îß§Ï∂úÎ™©Î°ù");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(1020, 120, 580, 340);
+		setBounds(1020, 120, 600, 390);
 		getContentPane().setLayout(null);
 		
 		menuTable.setRowHeight(38);
-		menuScroll.setBounds(12, 10, 549, 291);
+		menuScroll.setBounds(5, 5, 571, 338);
 		menuScroll.setViewportView(menuTable);
 		getContentPane().add(menuScroll);
-
-		setResizable(false);
+		
+		setResizable(true);
 		setVisible(true);
 	}
-
+	
 	public static void main(String[] args) {
 		Sales frame = new Sales();		
 	}
